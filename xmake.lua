946d7191dc81b81a3f9d519989269f9eb0af7939
Nicolas Jourdain <nicolas.jourdain@pm.me>
2026-09-26 00:20:42 +0200

    Clean first prototype

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,6 @@ set_version("0.1.0")
 set_languages("cxx23")
 set_warnings("all", "extra")
 
-if is_plat("windows") then
-    add_cxflags("/utf-8")
-end
-
 set_policy("build.warning", true)
 
 add_requires("raylib 6.0")
@@ -17,36 +13,38 @@ add_includedirs("src")
 
 -- Libraries
 
-target("vxcore")
-    set_kind("static")
-    add_files("src/vxcore/**.cpp")
-
-target("vxmodel")
-    set_kind("static")
-    add_deps("vxcore")
-    add_files("src/vxmodel/**.cpp")
-
-target("vxlayout")
-    set_kind("static")
-    add_deps("vxcore", "vxmodel")
-    add_files("src/vxlayout/**.cpp")
-
-target("vxrender")
-    set_kind("static")
-    add_deps("vxlayout")
-    add_packages("raylib")
-    add_files("src/vxrender/**.cpp")
+-- target("vxcore")
+--     set_kind("static")
+--     add_files("src/vxcore/**.cpp")
+--
+-- target("vxmodel")
+--     set_kind("static")
+--     add_deps("vxcore")
+--     add_files("src/vxmodel/**.cpp")
+--
+-- target("vxlayout")
+--     set_kind("static")
+--     add_deps("vxcore", "vxmodel")
+--     add_files("src/vxlayout/**.cpp")
+--
+-- target("vxrender")
+--     set_kind("static")
+--     add_deps("vxlayout")
+--     add_packages("raylib")
+--     add_files("src/vxrender/**.cpp")
 
 -- Binaries
 
 target("vitexxe")
     set_kind("binary")
-    add_deps("vxcore", "vxmodel", "vxlayout", "vxrender")
+    -- add_deps("vxcore", "vxmodel", "vxlayout", "vxrender")
+    add_deps()
     add_packages("raylib")
     add_files("src/vitexxe/**.cpp")
 
 target("vitexxe-tests")
     set_kind("binary")
-    add_deps("vxcore", "vxmodel", "vxlayout") -- should be the same as vitexxe deps above
+    -- add_deps("vxcore", "vxmodel", "vxlayout") -- should be the same as vitexxe deps above
+    add_deps()
     add_packages("catch2")
     add_files("tests/**.cpp")
